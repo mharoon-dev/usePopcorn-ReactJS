3b02d2858db8f8363959f5e0a1ca1510b1a1edde
mharoon-dev <haroon.aghani@gmail.com>
2024-03-05 23:30:31 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,18 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import MoviesList from "./Components/MoviesList/MoviesList.jsx";
 import MoviesDescription from "./Components/MoviesDescription/MoviesDescription.jsx";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(async () => {
+    let res = await fetch("http://www.omdbapi.com/?apikey=5ceed8f2&${search}");
+  }, [search]);
+
+  const [search, setSearch] = useState("");
   return (
     <>
-      <Navbar />
+      <Navbar search={search} setSearch={setSearch} />
       <div className="container">
         <div className="row">
           <MoviesList />
--- src/Components/MoviesList/MoviesList.jsx
+++ src/Components/MoviesList/MoviesList.jsx
@@ -2,12 +2,18 @@ import "./MoviesList.css";
 export default function MoviesList() {
   return (
     <>
-      <div className="offset-1 col-md-4 col-6 px-0 py-2" style={{ backgroundColor: "#24292c",borderRadius: "10px" }}>
-
-
+      <div
+        className="offset-1 col-md-4 col-6 px-0 py-2"
+        style={{ backgroundColor: "#24292c", borderRadius: "10px" }}
+      >
         <div
           className="card d-flex justify-content-start align-items-start "
-          style={{border: "none",borderRadius: "0px", backgroundColor: "transparent",borderBottom: "1px solid #353535" }}
+          style={{
+            border: "none",
+            borderRadius: "0px",
+            backgroundColor: "transparent",
+            borderBottom: "1px solid #353535",
+          }}
         >
           <div className="d-flex justify-content-start align-items-center">
             <img
@@ -29,137 +35,6 @@ export default function MoviesList() {
             </div>
           </div>
         </div>
-        <div
-          className="card d-flex justify-content-start align-items-start "
-          style={{border: "none",borderRadius: "0px", backgroundColor: "transparent",borderBottom: "1px solid #353535" }}
-        >
-          <div className="d-flex justify-content-start align-items-center">
-            <img
-              src="https://picsum.photos/200"
-              width={40}
-              height={60}
-              style={{ fitObject: "cover", margin: "5px" }}
-              alt="Profile Picture"
-              className="profile-pic ms-4 my-3"
-            />
-            <div>
-              <h6 className="profile-name p-0 ps-1 m-0 text-white">John Doe</h6>
-              <p
-                className="profile-name p-0 ps-1 m-0 text-white"
-                style={{ fontSize: "12px" }}
-              >
-                2006
-              </p>
-            </div>
-          </div>
-        </div>
-        <div
-          className="card d-flex justify-content-start align-items-start "
-          style={{border: "none",borderRadius: "0px", backgroundColor: "transparent",borderBottom: "1px solid #353535" }}
-        >
-          <div className="d-flex justify-content-start align-items-center">
-            <img
-              src="https://picsum.photos/200"
-              width={40}
-              height={60}
-              style={{ fitObject: "cover", margin: "5px" }}
-              alt="Profile Picture"
-              className="profile-pic ms-4 my-3"
-            />
-            <div>
-              <h6 className="profile-name p-0 ps-1 m-0 text-white">John Doe</h6>
-              <p
-                className="profile-name p-0 ps-1 m-0 text-white"
-                style={{ fontSize: "12px" }}
-              >
-                2006
-              </p>
-            </div>
-          </div>
-        </div>
-        <div
-          className="card d-flex justify-content-start align-items-start "
-          style={{border: "none",borderRadius: "0px", backgroundColor: "transparent",borderBottom: "1px solid #353535" }}
-        >
-          <div className="d-flex justify-content-start align-items-center">
-            <img
-              src="https://picsum.photos/200"
-              width={40}
-              height={60}
-              style={{ fitObject: "cover", margin: "5px" }}
-              alt="Profile Picture"
-              className="profile-pic ms-4 my-3"
-            />
-            <div>
-              <h6 className="profile-name p-0 ps-1 m-0 text-white">John Doe</h6>
-              <p
-                className="profile-name p-0 ps-1 m-0 text-white"
-                style={{ fontSize: "12px" }}
-              >
-                2006
-              </p>
-            </div>
-          </div>
-        </div>
-        <div
-          className="card d-flex justify-content-start align-items-start "
-          style={{border: "none",borderRadius: "0px", backgroundColor: "transparent",borderBottom: "1px solid #353535" }}
-        >
-          <div className="d-flex justify-content-start align-items-center">
-            <img
-              src="https://picsum.photos/200"
-              width={40}
-              height={60}
-              style={{ fitObject: "cover", margin: "5px" }}
-              alt="Profile Picture"
-              className="profile-pic ms-4 my-3"
-            />
-            <div>
-              <h6 className="profile-name p-0 ps-1 m-0 text-white">John Doe</h6>
-              <p
-                className="profile-name p-0 ps-1 m-0 text-white"
-                style={{ fontSize: "12px" }}
-              >
-                2006
-              </p>
-            </div>
-          </div>
-        </div>
-        <div
-          className="card d-flex justify-content-start align-items-start "
-          style={{border: "none",borderRadius: "0px", backgroundColor: "transparent",borderBottom: "1px solid #353535" }}
-        >
-          <div className="d-flex justify-content-start align-items-center">
-            <img
-              src="https://picsum.photos/200"
-              width={40}
-              height={60}
-              style={{ fitObject: "cover", margin: "5px" }}
-              alt="Profile Picture"
-              className="profile-pic ms-4 my-3"
-            />
-            <div>
-              <h6 className="profile-name p-0 ps-1 m-0 text-white">John Doe</h6>
-              <p
-                className="profile-name p-0 ps-1 m-0 text-white"
-                style={{ fontSize: "12px" }}
-              >
-                2006
-              </p>
-            </div>
-          </div>
-        </div>
-
-        
-
-
-
-
-
-
-        
-
-
 
       </div>
     </>
